19a05936d9c5bece5fe3b4e11904d91c493fa037
shahroz17 <shahroz.17@live.com>
2023-08-08 17:44:31 +0200

    Data ingestion stage completed

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Coccidiosis-Disease-Classification"
+AUTHOR_USER_NAME = "shahroz17"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "shahroz.17@gmail.com"
 
 
 setuptools.setup(
